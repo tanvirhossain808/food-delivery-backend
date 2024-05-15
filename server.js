eebdf683a9e5d60b1b949efbacc4760d3d47b4a9
tanvirhossain10 <tanvirhossen808@gmail.com>
2024-05-16 03:27:10 +0600

    add user can register through the backend check register

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
-
+import userRouter from "./routes/userRoute.js"
+import "dotenv/config"
 
 
 
@@ -27,13 +28,10 @@ app.use("/images", express.static("uploads"))
 app.get("/", (req, res) => {
     res.send("Testing server")
 })
-
+app.use("/api/user", userRouter)
 
 app.listen(port, () => {
     console.log(`Server is runnindg http://localhose:${port}`)
 })
 
 
-//mongodb+srv://tanvirhossan528:c2NmCS93HUJmefze@cluster0.i3lywhp.mongodb.net/?
-
-//c2NmCS93HUJmefze
