16060fbc46da68bab60461b9b310ceb6dbe3e89b
tanvirhossain10 <tanvirhossen808@gmail.com>
2024-05-16 17:41:13 +0600

    add logic for add to cart remove from the cart get cart for the backend

--- models/userModel.js
+++ models/userModel.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema(
         name: { type: String, required: true },
         email: { type: String, required: true, unique: true },
         password: { type: String, required: true },
-        carData: { type: Object, default: {} }
+        cartData: { type: Object, default: {} }
     }, { minimize: false }
 )
 
