29f82b9b665f274432486210a106278af26bbc3c
tanvirhossain10 <tanvirhossen808@gmail.com>
2024-05-17 16:00:47 +0600

    add backend code

--- controllers/ordersControllers.js
+++ controllers/ordersControllers.js
@@ -111,6 +111,40 @@ const userOrder = async (req, res) => {
 
 }
 
-export { verifyOrder, userOrder }
+//Listing orders for admin panel
+const listOrders = async (req, res) => {
+    try {
+        const orders = await orderModel.find({})
+        res.json({
+            success: true,
+            data: orders
+        })
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: "Error" })
+    }
+
+
+}
+
+//api for updating order status
+const updateStatus = async (req, res) => {
+    try {
+        await orderModel.findByIdAndUpdate(req.body.orderId, { status: req.body.status })
+        res.json({
+            success: true,
+            message: "Status Updated"
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.json({
+            success: false,
+            message: "Error"
+        })
+    }
+}
+
+export { verifyOrder, userOrder, listOrders, updateStatus }
 
 export default placeOrder
--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 import authMiddleware from "../middleware/auth.js";
-import placeOrder, { userOrder, verifyOrder } from "../controllers/ordersControllers.js";
+import placeOrder, { listOrders, updateStatus, userOrder, verifyOrder } from "../controllers/ordersControllers.js";
 
 
 const orderRouter = express.Router()
@@ -8,5 +8,7 @@ const orderRouter = express.Router()
 orderRouter.post("/place", authMiddleware, placeOrder)
 orderRouter.post("/verify", verifyOrder)
 orderRouter.post("/userorders", authMiddleware, userOrder)
+orderRouter.get("/list", listOrders)
+orderRouter.post("/status", updateStatus)
 
 export default orderRouter
