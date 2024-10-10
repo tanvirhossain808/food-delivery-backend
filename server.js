83ee1d1ff7d1aab18daadfc0f97c45763cf82379
Md. Tanvir Hossain <151984579+tanvirhossain808@users.noreply.github.com>
2024-10-10 15:05:58 +0600

    solving error

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 import express from "express"
 import cors from "cors"
-import { connectDB } from "./config/db.js"
-import foodRouter from "./routes/foodRoute.js"
-import userRouter from "./routes/userRoute.js"
+// import { connectDB } from "./config/db.js"
+// import foodRouter from "./routes/foodRoute.js"
+// import userRouter from "./routes/userRoute.js"
 import "dotenv/config"
-import cartRouter from "./routes/cartRoute.js"
-import orderRouter from "./routes/orderRoute.js"
+// import cartRouter from "./routes/cartRoute.js"
+// import orderRouter from "./routes/orderRoute.js"
 
 //app config
 
