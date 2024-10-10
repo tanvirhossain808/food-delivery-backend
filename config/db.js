7ddd34d4597aa091a6cf3538c214801c8c232362
Md. Tanvir Hossain <151984579+tanvirhossain808@users.noreply.github.com>
2024-10-10 11:33:52 +0600

    add backend vercel json

--- config/db.js
+++ config/db.js
@@ -1,7 +1,9 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"
 
 export const connectDB = async () => {
-    console.log("hey");
-    await mongoose.connect('mongodb+srv://tanvirhossan528:c2NmCS93HUJmefze@cluster0.i3lywhp.mongodb.net/foodDelivery')
-        .then(() => console.log('db conncected'))
+    await mongoose
+        .connect(
+            "mongodb+srv://tanvirhossan528:c2NmCS93HUJmefze@cluster0.i3lywhp.mongodb.net/foodDelivery"
+        )
+        .then(() => console.log("db conncected"))
 }
