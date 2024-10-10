b9f65ecfd3d35d1c29ae87ad09be3bce6e7d6da7
Md. Tanvir Hossain <151984579+tanvirhossain808@users.noreply.github.com>
2024-10-10 15:02:12 +0600

    solving bugs

--- server.js
+++ server.js
@@ -10,36 +10,48 @@ import orderRouter from "./routes/orderRoute.js"
 //app config
 
 const app = express()
-const port = process.env.post || 8081
+const port = 3000
 
 //middleware
 
 app.use(express.json())
 app.use(cors())
+const PORT = process.env.PORT || 4000
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
 
 //db connection
 
-connectDB()
-    .then(() => {
-        app.listen(port, () => {
-            try {
-                console.log(`Server is running http://localhose:${port}`)
-            } catch (error) {
-                console.log(error)
-            }
-        })
-    })
-    .catch((err) => console.log(err))
+// connectDB()
+//     .then(() => {
+//         app.listen(port, () => {
+//             try {
+//                 console.log(`Server is running http://localhose:${port}`)
+//             } catch (error) {
+//                 console.log(error)
+//             }
+//         })
+//     })
+// .catch((err) => console.log(err))
 
 //api endpoints
-app.use("/api/food", foodRouter)
-app.use("/images", express.static("uploads"))
-app.use("/api/user", userRouter)
-app.use("/api/cart", cartRouter)
-app.use("/api/order", orderRouter)
+// app.use("/api/food", foodRouter)
+// app.use("/images", express.static("uploads"))
+// app.use("/api/user", userRouter)
+// app.use("/api/cart", cartRouter)
+// app.use("/api/order", orderRouter)
 
-app.get("/", (req, res) => {
+app.use("/", (req, res) => {
     res.send("Testing server")
 })
 
 // export default app
+app.listen(port, () => {
+    // try {
+    //     console.log(`Server is running http://localhose:${port}`)
+    // } catch (error) {
+    //     console.log(error)
+    // }
+    console.log("server is ready")
+})
